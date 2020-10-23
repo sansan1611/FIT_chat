130cefa2709ea72d3e6f2cf5142b392aff68e5ca
Tran Thi Mai Huong <tranmaihuong@MacBook-Pro-cua-Tran.local>
2020-10-23 21:27:42 +0700

    first commit

---
+++ src/test/frame.java
@@ -0,0 +1,17 @@
+package test;
+
+import javax.swing.JFrame;
+
+public class frame {
+	public frame() {
+JFrame frame= new JFrame("Hello");
+frame.setSize(400,400);
+frame.setVisible(true);
+frame.setBackground(blue);
+}
+public static void main (String[]args) {
+	new frame();
+}
+
+
+}
